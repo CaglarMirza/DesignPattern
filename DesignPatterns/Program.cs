868732697adc1017edfabe57c942cb4fd36c5bff
Caglar <ahmetcmirza@gmail.com>
2023-05-16 23:44:40 +0200

    designpattern

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,22 @@
+﻿//using DesignPatterns;
+////using SingletonDesignPattern;
+//var singleton = Singleton.GetInstance(); 
+//singleton.ZegGoeiedag(); 
+//var tweedeSingleton = Singleton.GetInstance(); 
+//if (singleton == tweedeSingleton) 
+//Console.WriteLine("Zelfde instanties");
+//tweedeSingleton.ZegGoeiedag();
+//Console.ReadLine();
+
+
+
+using System.Text;
+using DesignPatterns;
+
+var bak = Versnellingsbak.GetInstance();
+bak.GaNaarVersnelling(1);
+bak.GaNaarVersnelling(7);
+//als test een tweede verwijzing naar dezelfde versnellingsbak
+var bak2 = Versnellingsbak.GetInstance();
+bak2.GaNaarVersnelling(0);
+bak2.GaNaarVersnelling(-1);
---
+++ DesignPatterns/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace DesignPatterns
+{
+    public class Versnellingsbak
+    {
+        private static Versnellingsbak? _uniekeInstantie;
+        private int versnelling;
+        private Versnellingsbak() { }
+        public static Versnellingsbak GetInstance()
+        {
+            if (_uniekeInstantie == null)
+            {
+                _uniekeInstantie = new Versnellingsbak();
+            }
+            return _uniekeInstantie;
+        }
+        public void GaNaarVersnelling(int versnelling)
+        {
+            if ((versnelling < -1) || (versnelling > 5))
+                Console.WriteLine("Verkeerde versnelling : {0}", versnelling);
+            else
+            {
+                if (versnelling == -1)
+                    Console.WriteLine("Van {0}e versnelling in achteruit",
+                        this.versnelling);
+                else
+                    this.versnelling = versnelling;
+            }
+        }
+    }
+}
+
